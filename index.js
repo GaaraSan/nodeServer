3e7d439deda572218d33138e46ab4c260591a33c
GaaraSan <prosto.artem158@gmail.com>
2022-10-24 15:43:35 +0300

    Rest+Mongo

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express"),
         app = express(),
-        hostname = '127.0.0.1',
+        hostname = "127.0.0.1",
         port = 3000,
-        routes = require("./routes.js");
+        routes = require("./routes.js"),
+        mongoose = require("mongoose"),
+        User = require("./models/UserModel");
+
+mongoose.connect("mongodb://localhost/users_db");
 
 routes(app);
 app.listen(port, hostname, ()=>{
--- routes.js
+++ routes.js
@@ -1,13 +1,19 @@
-const users = [{name:"user1", age:"16"},
-                {name:"user2", age:"18"},
-                {name:"user3", age:"17"}];
+// const users = [{name:"user1", age:"16"},
+//                 {name:"user2", age:"18"},
+//                 {name:"user3", age:"17"}];
+
+User = require("./models/UserModel")
 
 module.exports = function(app){
     app.get("/", (req, res)=>{
-        res.send("<h1>Hello</h1>");
-    });
+        User.find((err, users)=>{
+            if(err)
+                res.send(err);
+            res.json(users);
+        });
+    })
     
-    app.get("/users", (req, res)=>{
-        res.json(users);
+    app.post("/", (req, res)=>{
+        res.send(users);
     })
 }                
